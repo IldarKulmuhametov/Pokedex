96607aac4452e8ec10908dcde1db964d7310e0ce
Ildar <ildar.kulmuhametov@devico.io>
2016-04-11 00:33:49 +0300

    fixed paths

--- server/app.js
+++ server/app.js
@@ -5,11 +5,36 @@ import path from 'path'
 
 
 const app = express();
-app.use(express.static(path.join(__dirname, '../public')));
+//app.use(express.static(path.join(__dirname, '../public')));
 
-
-app.get("*", (req, res, next) => {
-    res.sendFile(path.join(__dirname, '../public/index.html'))
+app.use('/src/styles/main.css ', function(req,res){
+    res.sendFile(path.resolve('/public/src/styles/main.css'))
 });
+
+app.use('/src/build/build.js', function(req,res){
+    res.sendFile(path.resolve('/public/src/build/build.js'))
+});
+
+
+app.use('/src/packages/bootstrap-material-design/dist/css/material.css ', function(req,res){
+    res.sendFile(path.resolve('/public/src/packages/bootstrap-material-design/dist/css/material.css '))
+});
+
+
+
+app.use('/src/packages/bootstrap/dist/css/bootstrap.css', function(req,res){
+    res.sendFile(path.resolve('/public/src/packages/bootstrap/dist/css/bootstrap.css'))
+});
+
+
+
+app.get('/public', function (req, res) {
+    res.sendFile(path.resolve('index.html'));
+});
+
+
+//app.get("*", (req, res, next) => {
+//    res.sendFile(path.join(__dirname, '../public/index.html'))
+//});
 
 config.application.ports.map(port => app.listen(port))
